1d757fa1b3da47cc7ac7ba14adcfc4f438ef71d4
Dragontek Software <joshua.mondragon@dragontek.com>
2018-11-23 08:29:10 -0700

    Update lib

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,11 @@
-export * from './angularparse.module';
-export * from './angularparse';
-export * from './interfaces';
-export * from './object/object';
-export * from './query/query';
+/*
+ * Public API Surface of angularparse
+ */
+
+export * from './lib/angularparse.service';
+export * from './lib/auth-guard.service';
+export * from './lib/angularparse.module';
+export * from './lib/interfaces';
+export * from './lib/object/object';
+export * from './lib/query/query';
+export * from './lib/flatten';
